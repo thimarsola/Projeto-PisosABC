33157cd4f887a337f8d76bd891e1baf6bef360b0
Marsola <tmarsola94@gmail.com>
2022-03-18 11:36:06 -0300

    Create a footer

---
+++ template-parts/footer/footer-copyright.php
@@ -0,0 +1,5 @@
+<div class="footer__copyright bg-gray-700 py-1 text-white-500 t-center">
+    <div class="container">
+        <small>&COPY; <?= (SITE['name'] . " - " . date('Y') . '.'); ?> Todos os direitos reservados</small>
+    </div>
+</div>
